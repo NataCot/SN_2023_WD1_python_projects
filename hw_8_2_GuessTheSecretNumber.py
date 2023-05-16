c8684466b8b0b1ac80d54370c9e5c52248ba5bc5
Natasa <43881848+NataCot@users.noreply.github.com>
2023-05-16 21:03:19 +0200

    Initial commit

--- hw_8_2_GuessTheSecretNumber.py
+++ hw_8_2_GuessTheSecretNumber.py
@@ -1,1 +1,13 @@
+import random
+secret = random.randint(1, 100)
+guess = input("Guess the number: ")
+guess = int(guess)
+if secret == guess:
+    print("Congratulations!")
+elif secret != guess:
+    if secret < guess:
+        print("Wrong! The number you entered is too big.")
+    else:
+        print("Wrong! The number you entered is too small.")
+    print("The right number is " + str(secret))
 
