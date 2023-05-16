863ff73b8b011aa8f9bb9a156f6f51b64a7c1453
Natasa <43881848+NataCot@users.noreply.github.com>
2023-05-16 20:12:00 +0200

    Initial commit

---
+++ sn2023_hw_8_1.py
@@ -0,0 +1,16 @@
+mood = input("Enter your mood: ")
+
+if mood == "happiness":
+    print("It's great to see you happy.")
+elif mood == "surprise":
+    print("Good heavens! I didn't expect that.")
+elif mood == "confused":
+    print("Center yourself and firmly state: I don't know what to do, and that is ok!")
+elif mood == "worried":
+    print("Don't worry, it'll get better.")
+elif mood == "angry":
+    print("Take a deep breath and relax.")
+elif mood == "sad":
+    print("Cheer up, there are sunnier days ahead!")
+else:
+    print("I don't recognize this mood")
