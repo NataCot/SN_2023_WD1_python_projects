694d7b87694d7a772a5b5c050d8aa0cc8804ee30
Natasa <43881848+NataCot@users.noreply.github.com>
2023-05-19 14:01:06 +0200

    With only one value x

---
+++ hw_9_2_FizzBuzz.py
@@ -0,0 +1,12 @@
+print("Welcome to FizzBuzz!")
+x = int(input("Please select a whole number between 1 and 100: "))
+
+for x in range(1, x + 1):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
